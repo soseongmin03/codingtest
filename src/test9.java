ef66cce3f1457cf2021fef46724062745a1d6063
soseongmin03 <sosungmin0122@gmail.com>
2026-01-05 17:37:44 +0900

    2026-01-05

--- src/test9.java
+++ src/test9.java
@@ -1,2 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
 public class test9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> myqueue = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if(first_abs == second_abs){ // 절댓값이 같음
+                return o1> o2 ? 1 : -1;
+            }
+            return first_abs - second_abs; // 리턴값이 양수면 뒤에값이 작음 음수면 앞의값이 작음
+        });
+        for(int i = 0; i<n ; i++){
+            int request = Integer.parseInt(br.readLine());
+            if(request == 0){
+                if(myqueue.isEmpty()){
+                    System.out.println("0");
+                }else{
+                    System.out.println(myqueue.poll());
+                }
+            }else{
+                myqueue.add(request);
+            }
+        }
+    }
 }
